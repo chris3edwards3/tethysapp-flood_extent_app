e810cb1babae15b09bdaef86fac7f99bcce4b101
Corey Krewson <coreykrewson@gmail.com>
2018-07-25 15:35:08 -0600

    Update main.js

--- tethysapp/flood_extent_app/public/js/main.js
+++ tethysapp/flood_extent_app/public/js/main.js
@@ -20,7 +20,7 @@ var Legend = L.control({
 });
 
 Legend.onAdd = function(map) {
-    var src= "https://tethys.byu.edu/thredds/wms/testAll/floodextent/floodedscale.nc?REQUEST=GetLegendGraphic&LAYER=Height&PALETTE=redblue&COLORSCALERANGE=0,40";
+    var src= "https://tethys.byu.edu/thredds/wms/testAll/floodextent/floodedscale.nc?REQUEST=GetLegendGraphic&LAYER=Height&PALETTE=whiteblue&COLORSCALERANGE=0,40";
     var div = L.DomUtil.create('div', 'info legend');
     div.innerHTML +=
         '<img src="' + src + '" alt="legend">';
