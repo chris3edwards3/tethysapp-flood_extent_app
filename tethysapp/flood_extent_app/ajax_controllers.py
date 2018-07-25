cd7dc574ff4e7862873a296b82d271e0db9e2043
Corey Krewson <coreykrewson@gmail.com>
2018-07-25 16:57:02 -0600

    Update ajax_controllers.py

--- tethysapp/flood_extent_app/ajax_controllers.py
+++ tethysapp/flood_extent_app/ajax_controllers.py
@@ -254,97 +254,105 @@ def createprobnetcdf(request):
     res = requests.get(host,
                        params=request_params,
                        headers=request_headers)
+    
+    if res.content == 'Invalid data: Invalid ECMWF Forecast file ...':
+        
+        return_obj = {'success':True,'message':"no streamflows available for this date"}
+        
+        return JsonResponse(return_obj)
+    
+    else:
 
-    flows = res.content.splitlines()
+        flows = res.content.splitlines()
 
-    flowlist = []
-    times = []
+        flowlist = []
+        times = []
 
-    for flow in flows:
-        flowlist.append(flow.split(","))
+        for flow in flows:
+            flowlist.append(flow.split(","))
 
-    catchfloods = xarray.open_dataset(catchfloodnetcdf, autoclose=True).nepalcatchproj
-    hand = xarray.open_dataset(handnetcdf, autoclose=True).nepalhandproj
+        catchfloods = xarray.open_dataset(catchfloodnetcdf, autoclose=True).nepalcatchproj
+        hand = xarray.open_dataset(handnetcdf, autoclose=True).nepalhandproj
 
-    # scaling down grid netcdf to specific gridid
-    catlat = catchfloods.where(catchfloods.values == gridid).dropna('lat', how='all')
-    gridonly = catlat.where(catlat.values == gridid).dropna('lon', how='all')
+        # scaling down grid netcdf to specific gridid
+        catlat = catchfloods.where(catchfloods.values == gridid).dropna('lat', how='all')
+        gridonly = catlat.where(catlat.values == gridid).dropna('lon', how='all')
 
-    lats = gridonly.lat.values
-    lons = gridonly.lon.values
+        lats = gridonly.lat.values
+        lons = gridonly.lon.values
 
-    # scaling down hand netcdf to specific gridid size
-    handsmall = hand.sel(lat=slice(lats[0], lats[-1] - (lats[0] - lats[1])))
-    handsmall = handsmall.sel(lon=slice(lons[0], lons[-1]))
+        # scaling down hand netcdf to specific gridid size
+        handsmall = hand.sel(lat=slice(lats[0], lats[-1] - (lats[0] - lats[1])))
+        handsmall = handsmall.sel(lon=slice(lons[0], lons[-1]))
 
-    del flowlist[0]
+        del flowlist[0]
 
-    for vals in flowlist:
-        times.append(vals[0])
+        for vals in flowlist:
+            times.append(vals[0])
 
-    for timestep in range(0, len(flowlist)):
+        for timestep in range(0, len(flowlist)):
 
-        print(str(timestep + 1) + " out of " + str(len(flowlist)))
+            print(str(timestep + 1) + " out of " + str(len(flowlist)))
 
-        heights = []
+            heights = []
 
-        for b in range(1, len(flowlist[timestep])):
-            flow = (float(flowlist[timestep][b]))
-            if flow == 0:
-                H = -1.0
+            for b in range(1, len(flowlist[timestep])):
+                flow = (float(flowlist[timestep][b]))
+                if flow == 0:
+                    H = -1.0
+                else:
+                    H = 0.0
+                if flow > minQ:
+                    H = float(gridcurve.loc[gridcurve['Q'] > flow, 'H'].iloc[0]) - 1
+                    heights.append(H)
+                else:
+                    heights.append(H)
+
+            flooded = handsmall.to_dataset()
+
+            index = 0
+            height = heights[index]
+            flooded['timeseries'] = gridonly
+            flooded.timeseries.values = gridonly.where(gridonly != gridid, height).values
+            flooded.timeseries.values = xarray.where(flooded.timeseries >= flooded.nepalhandproj, flooded.nepalhandproj, np.nan).values
+            floodedarray = flooded.timeseries.expand_dims('time', axis=2).to_masked_array()
+            oldheight = ''
+
+            for index in range(1, len(heights)):
+                if heights[index] == oldheight:
+                    floodlen = len(floodedarray[0][0])
+                    floodedarray = np.insert(floodedarray, floodlen, floodedvalues, axis=2)
+                else:
+                    height = heights[index]
+                    floodlen = len(floodedarray[0][0])
+                    flooded['step'] = gridonly
+                    flooded.step.values = gridonly.where(gridonly != gridid, height).values
+                    floodedvalues = xarray.where(flooded.step >= flooded.nepalhandproj, flooded.nepalhandproj, np.nan).values
+                    floodedarray = np.insert(floodedarray, floodlen, floodedvalues, axis=2)
+                    flooded.__delitem__('step')
+                    oldheight = height
+
+            non_nans = (~np.isnan(floodedarray)).sum(2)
+            floodedprob = np.where(non_nans == 0, np.nan, non_nans) / 51 * 100
+
+            if timestep == 0:
+                totfloodedprob = np.expand_dims(floodedprob, axis=2)
             else:
-                H = 0.0
-            if flow > minQ:
-                H = float(gridcurve.loc[gridcurve['Q'] > flow, 'H'].iloc[0]) - 1
-                heights.append(H)
-            else:
-                heights.append(H)
+                floodproblen = len(totfloodedprob[0][0])
+                totfloodedprob = np.insert(totfloodedprob, floodproblen, floodedprob, axis=2)
 
-        flooded = handsmall.to_dataset()
+        times = pd.to_datetime(times)
 
-        index = 0
-        height = heights[index]
-        flooded['timeseries'] = gridonly
-        flooded.timeseries.values = gridonly.where(gridonly != gridid, height).values
-        flooded.timeseries.values = xarray.where(flooded.timeseries >= flooded.nepalhandproj, flooded.nepalhandproj, np.nan).values
-        floodedarray = flooded.timeseries.expand_dims('time', axis=2).to_masked_array()
-        oldheight = ''
+        ds = xarray.Dataset({'Flood Probability': (['lat', 'lon', 'times'], totfloodedprob)},
+                            coords={'lon': lons,
+                                    'lat': lats,
+                                    'times': times})
 
-        for index in range(1, len(heights)):
-            if heights[index] == oldheight:
-                floodlen = len(floodedarray[0][0])
-                floodedarray = np.insert(floodedarray, floodlen, floodedvalues, axis=2)
-            else:
-                height = heights[index]
-                floodlen = len(floodedarray[0][0])
-                flooded['step'] = gridonly
-                flooded.step.values = gridonly.where(gridonly != gridid, height).values
-                floodedvalues = xarray.where(flooded.step >= flooded.nepalhandproj, flooded.nepalhandproj, np.nan).values
-                floodedarray = np.insert(floodedarray, floodlen, floodedvalues, axis=2)
-                flooded.__delitem__('step')
-                oldheight = height
+        ds.to_netcdf(thredds + 'prob' + str(gridid) + '.nc')
 
-        non_nans = (~np.isnan(floodedarray)).sum(2)
-        floodedprob = np.where(non_nans == 0, np.nan, non_nans) / 51 * 100
+        return_obj = {'success':True,'gridid':gridid}
 
-        if timestep == 0:
-            totfloodedprob = np.expand_dims(floodedprob, axis=2)
-        else:
-            floodproblen = len(totfloodedprob[0][0])
-            totfloodedprob = np.insert(totfloodedprob, floodproblen, floodedprob, axis=2)
-
-    times = pd.to_datetime(times)
-
-    ds = xarray.Dataset({'Flood Probability': (['lat', 'lon', 'times'], totfloodedprob)},
-                        coords={'lon': lons,
-                                'lat': lats,
-                                'times': times})
-
-    ds.to_netcdf(thredds + 'prob' + str(gridid) + '.nc')
-
-    return_obj = {'success':True,'gridid':gridid}
-
-    return JsonResponse(return_obj)
+        return JsonResponse(return_obj)
 
 def getdates(request):
     return_obj = {
